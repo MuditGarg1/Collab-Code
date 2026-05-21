3a57b3771da3424080a5baf4d9cd22606b21dee0
Mudit Garg <muditga3333@gmail.com>
2026-05-21 14:09:27 +0530

    Fix circular dependency in axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { ServerUrl } from "../App";
+
+const ServerUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
 
 const api = axios.create({
   baseURL: `${ServerUrl}/api`,
